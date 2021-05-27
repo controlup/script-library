7ae671cd2aa88af1c2f1732327d0989036571c0d
Joel Stocker <joel@scriptbased.com>
2021-05-27 07:13:27 -0400

    Sync 052721

---
+++ Clear Old SCCM Cache/Clear Old SCCM Cache.ps1
@@ -0,0 +1,42 @@
+﻿<#
+ 	.SYNOPSIS
+        A simple script that clears all items older than 7 days in the SCCM update cache.
+		
+    .DESCRIPTION
+        Since Microsoft changed thier approach to Windows Updates, the size of the patches has increased significantly. This can be
+        challenging to manage on persistent machines with limited disk space. This script is designed to clear that cache to free up
+        some valuable disk space.
+    
+    .NOTES
+        The scrips perform a check to ensure the require COM object exists. If it does not exist, no action will be taken.
+		
+    .LINK
+        For more information refer to:
+            http://www.controlup.com
+
+    .LINK
+        Stay in touch:
+        http://twitter.com/rorymon
+
+    .EXAMPLE
+        C:\PS>\. ClearSCCMCache.ps1
+		
+		Clears SCCM Update cache items older than 7 days.
+#>
+
+## Last modified 13:33 GMT 20/04/21 @rorymon
+
+$resman = new-object -com "UIResource.UIResourceMgr"
+$cacheInfo = $resman.GetCacheInfo()
+
+if ($resman) {
+$cacheinfo.GetCacheElements()  | 
+where-object {$_.LastReferenceTime -lt (get-date).AddDays(-7)} | 
+foreach {
+$cacheInfo.DeleteCacheElement($_.CacheElementID)
+}
+} else {
+write-host ("Required COM Object Does Not Exist. Ensure SCCM Client is Installed.")
+}
+
+
---
+++ Clear Old SCCM Cache/README.md
@@ -0,0 +1,14 @@
+﻿# Name: Clear Old SCCM Cache
+
+Description: Clears all content older than 7 days
+
+Version: 1.1.7
+
+Creator: Rory.Monaghan
+
+Date Created: 11/28/2018 08:48:56
+
+Date Modified: 04/20/2021 12:48:06
+
+Scripting Language: ps1
+
